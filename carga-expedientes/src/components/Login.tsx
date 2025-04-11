efeade889939b21e5bf2a55f38f5590e2e41e942
xleox2211 <leonardojesusgarcia8@gmail.com>
2025-04-11 19:34:21 -0400

    se agrego el login

--- carga-expedientes/src/components/Login.tsx
+++ carga-expedientes/src/components/Login.tsx
@@ -82,7 +82,6 @@ function Login() {
           </div>
         </div>
 
-        {/* Botón de Submit */}
         <button
           type="submit"
           className="w-full bg-gradient-to-r from-blue-600 to-blue-500 hover:from-blue-700 hover:to-blue-600 text-white py-2 sm:py-3 px-4 rounded-lg font-semibold shadow-md transition duration-200 transform hover:scale-[1.02] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 text-sm sm:text-base"
