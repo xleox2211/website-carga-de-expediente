3b230cf637653958146bc7e1ec86137e9ea6f445
Sergio Ricaflor <sergioricaflor@gmail.com>
2025-06-01 11:02:56 -0400

    fix: cambio de lugar donde se guarda la base de datos

--- backend/src/models/DB.ts
+++ backend/src/models/DB.ts
@@ -2,7 +2,8 @@ import { Sequelize } from 'sequelize';
 
 const DataBase = new Sequelize({
   dialect: 'sqlite',
-  storage: './database.sqlite',
+  storage: './db/db.sqlite',
+  logging: false, // Disable logging for cleaner output
 });
 
 export default DataBase;
